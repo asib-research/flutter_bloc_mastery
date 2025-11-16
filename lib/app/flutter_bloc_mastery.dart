6380e801104e47830756e1f44656316785d8775d
dev-asib <asib.professional@gmail.com>
2025-11-16 17:09:47 +0600

    feat(counter): implement basic counter demo using Cubit architecture
    - Added CounterCubit with increment and decrement logic
    - Integrated BlocProvider at app root level
    - Implemented CounterScreen with BlocBuilder to react to state changes
    - Added UI controls for increment/decrement actions
    - Ensured clean separation of concerns and minimal boilerplate

---
+++ lib/app/flutter_bloc_mastery.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_mastery/cubits/counter_cubit.dart';
+import 'package:flutter_bloc_mastery/screens/counter_screen.dart';
+class FlutterBlocMastery extends StatelessWidget {
+  const FlutterBlocMastery({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(create: (_)=> CounterCubit(), child: CounterScreen(),),
+    );
+  }
+}
---
+++ lib/screens/counter_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_mastery/cubits/counter_cubit.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter with Cubit")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocBuilder<CounterCubit, int>(
+              builder: (context, counterCubit) {
+                return Text(
+                  "Count: $counterCubit",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black,
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: ()  {
+                context.read<CounterCubit>().increment();
+              },
+              child: Text("Tap"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
